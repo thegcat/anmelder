c04a913bb36a425bf16cb59ea6ada86878574bd3
Felix Schäfer <felix@thegcat.net>
2016-12-09 11:44:39 +0100

    Add simple_form and adapt event form
    * use optional instead of required
    * adapt error styling
    * add hints

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
   # slug is required, unique and something that might be in an URL path
   validates :slug, presence: true, format: { with: /\A[\p{Word}-]+\z/ }, uniqueness: { case_sensitive: false }
 
