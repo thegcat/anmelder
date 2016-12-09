ed4d051133ada5ca5ef5016946673efaff49f618
Felix Schäfer <felix@thegcat.net>
2016-12-09 20:44:07 +0100

    Don't allow the 'new' slug for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   validates :title, presence: true
   # slug is required, unique and something that might be in an URL path
-  validates :slug, presence: true, format: { with: /\A[\p{Word}-]+\z/ }, uniqueness: { case_sensitive: false }
+  validates :slug, presence: true, format: { with: /\A[\p{Word}-]+\z/ }, uniqueness: { case_sensitive: false }, exclusion: { in: %w(new) }
 
   # use slug for URL generation
   def to_param
